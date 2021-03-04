e83f8223f8d5dd10c5708bd119d3d3a560b066de
James Pease <peasejb@wfu.edu>
2021-03-04 16:36:21 -0500

    update to redirect

--- doc/faq.rst
+++
@@ -1,6 +0,0 @@
-Frequently Asked Questions
-==========================
-
-See also our forum at: https://groups.google.com/forum/#!forum/mvftools
-
-Coming soon.
--- examples/mvf_chromoplot.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-mvftools.py PlotChromoplot --mvf FILE.mvf.gz --samples A,B,C,D --outgroup OG,OG2 --windowsize 100000 --contigs 1,2,3 --yscale 10 --xscale 10
